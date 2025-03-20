165b938a33d4d403dd8fefe2ac8deb5d5c4656dd
Pauloglhr <pauloglhr@outlook.com>
2025-03-19 21:22:55 -0300

    Realizado refatoração de código com o objetivo de isolar lógica de negócio em um arquivo reducer.

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,21 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
+import { carrinhoReducer } from "@/reducers/carrinhoReducer";
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = "Carrinho";
+const estadoInicial = [];
 
 export const CarrinhoProvider = ({ children }) => {
-  const [carrinho, setCarrinho] = useState([]);
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidade, setQuantidade] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
+
+  //Reduz a array a um só valor. No caso o objetivo é extrair valores específicos dos itens da array carrinho.
+  const { quantidadeTemp, totalTemp } = useMemo(() => {
+    return carrinho.reduce(
+      (acumulador, produto) => ({
+        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+      }),
+      {
+        quantidadeTemp: 0,
+        totalTemp: 0,
+      }
+    );
+  }, [carrinho]);
+
+  useEffect(() => {
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  });
+
   return (
     <CarrinhoContext.Provider
       value={{
         carrinho,
-        setCarrinho,
+        dispatch,
         quantidade,
-        setQuantidade,
         valorTotal,
-        setValorTotal,
       }}
     >
       {children}
--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,92 +1,55 @@
+import { useContext, useMemo } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
+import {
+  ADD_PRODUTO,
+  REMOVE_PRODUTO,
+  UPDATE_QUANTIDADE,
+} from "../reducers/carrinhoReducer";
+
+const addProdutoAction = (novoProduto) => ({
+  type: ADD_PRODUTO,
+  payload: novoProduto,
+});
+
+const removeProdutoAction = (produtoId) => ({
+  type: REMOVE_PRODUTO,
+  payload: produtoId,
+});
+
+const updateQuantidadeAction = (produtoId, quantidade) => ({
+  type: UPDATE_QUANTIDADE,
+  payload: { produtoId, quantidade },
+});
 
 export const useCarrinhoContext = () => {
-  const {
-    carrinho,
-    setCarrinho,
-    quantidade,
-    setQuantidade,
-    valorTotal,
-    setValorTotal,
-  } = useContext(CarrinhoContext);
+  const { carrinho, dispatch, quantidade, valorTotal } =
+    useContext(CarrinhoContext);
 
-  const mudarQuantidade = (id, quantidade) => {
-    return carrinho.map((itemDoCarrinho) => {
-      if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
-      return itemDoCarrinho;
-    });
-  };
+  function adicionarProduto(novoProduto) {
+    dispatch(addProdutoAction(novoProduto));
+  }
 
-  const adicionarProduto = (novoProduto) => {
-    const temOProduto = carrinho.some(
-      (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
-    );
+  function removerProduto(id) {
+    const produto = carrinho.find((item) => item.id === id);
 
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto,
-      ]);
+    if (produto && produto.quantidade > 1) {
+      dispatch(updateQuantidadeAction(id, produto.quantidade - 1));
+    } else {
+      dispatch(removeProdutoAction(id));
     }
+  }
 
-    const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1);
-
-    setCarrinho([...carrinhoAtualizado]);
-  };
-
-  const removerProduto = (id) => {
-    //encontra o produto em questão.
-    //O método em questão, percorre a array até que a verificação resulte em true, atribuindo o valor do item iterado à variável.
-    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-
-    //verifica se é o último.
-    const ehOUltimo = produto.quantidade === 1;
-
-    //se for o último, remove da lista.
-    if (ehOUltimo) {
-      return setCarrinho((carrinhoAnterior) => {
-        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id);
-      });
-    }
-
-    //se não for o último, decresce a quantidade.
-    const carrinhoAtualizado = mudarQuantidade(id, -1);
-
-    setCarrinho([...carrinhoAtualizado]);
-  };
-
-  const removerProdutoCarrinho = (id) => {
-    const carrinhoAtualizado = carrinho.filter(
-      (itemDoCarrinho) => itemDoCarrinho.id !== id
-    );
-    setCarrinho(carrinhoAtualizado);
-  };
-
-  useEffect(() => {
-    //Reduz a array a um só valor. No caso o objetivo é extrair valores específicos dos itens da array carrinho.
-    const { quantidadeTemp, totalTemp } = carrinho.reduce(
-      (acumulador, produto) => ({
-        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
-        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
-      }),
-      {
-        quantidadeTemp: 0,
-        totalTemp: 0,
-      }
-    );
-    setQuantidade(quantidadeTemp);
-    setValorTotal(totalTemp);
-  }, [carrinho]);
+  function removerProdutoCarrinho(id) {
+    dispatch(removeProdutoAction(id));
+  }
 
   return {
     carrinho,
-    setCarrinho,
     adicionarProduto,
     removerProduto,
     removerProdutoCarrinho,
-    quantidade,
     valorTotal,
+    quantidade,
   };
 };
